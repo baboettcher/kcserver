d725c59d4726daf38d0d6ff06ec1aafb68028380
Adam Boettcher <baboettcher@gmail.com>
2018-09-08 14:26:37 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+//const graphqlHTTP = require("express-graphql");
+//const schema = require("./schema/schema");
+//const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+// middleware
+// allow cross-origin requests
+app.use(cors());
+
+const router = express.Router();
+
+app.get("/users", function(req, res, next) {
+  /*   res.json([{ id: 1, username: "samantha" }, { id: 2, username: "willy" }]);
+ */
+  console.log("USER");
+  res.send([{ 1: "She had rings on her fingers" }]);
+});
+
+app.listen(process.env.port || 4444, () => {
+  console.log("Now listening for Kidcoin3 requests on port 4444!!");
+});
+
+// options passed due to error. solution here:
+// https://stackoverflow.com/questions/30909492/mongoerror-topology-was-destroyed
+
+/* var options = {
+  server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
+  replset: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }
+}; */
